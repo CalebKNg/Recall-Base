2a897b89791dd7a8266c3fdcb1ee33f1d4cf6851
CalebKNg <caleb17ng@gmail.com>
2025-03-11 11:55:38 -0400

    Update function implementation

--- Recall/communication.py
+++ Recall/communication.py
@@ -5,8 +5,9 @@ from multiprocessing import Process, Queue
 import config
 
 class Object:
-    def __init__(self, id):
+    def __init__(self, id, name):
         self.id = id
+        self.name = name
         self.x = 0
         self.y = 0
 
@@ -41,10 +42,38 @@ class RecallApp:
 
         response = requests.get(url, headers=headers)
         if(response.status_code == 200):
-            self.trackedObjects = response.json()
+            for item in response.json():
+                id = item["id"]
+                name = item["name"]
+                self.trackedObjects.append(Object(id, name))
+            # self.trackedObjects = response.json()
 
-    def updateLocations(self, ID):
-        pass
+    def updateLocations(self, id, x, y):
+        for item in self.trackedObjects:
+            if item.id == id:
+                # calculate distance between last seen location
+                # euclidean distance
+                dist = np.sqrt((item.x - x)**2 + (item.y - y)^2)
+                
+
+                # Update distance
+                item.x = x
+                item.y = y
+
+
+
+    def sendUpdate(self, id, name, image, description):
+        url = "fydp-backend-production.up.railway.app/ObjectTracking/" + id
+        headers = {"Content-Type": "application/json", "Authorization":self.bearerToken}
+
+        data = {
+            "name": name,
+            "location_image": image,
+            "location_description": description
+        }
+
+        response = requests.post(url, json=data, headers=headers)
+        print(response.status_code)
 
     def run(self):
         # Run this loop in another process
