459edeff4717daa800e1216ca00909261e1f273a
CalebKNg <caleb17ng@gmail.com>
2025-03-11 23:45:08 -0400

    update location later

--- Recall/communication.py
+++ Recall/communication.py
@@ -74,7 +74,8 @@ class RecallApp:
                 yavg = ysum/len(item.locHistory)
 
                 # euclidean distance
-                dist = np.sqrt((item.x - xsum)**2 + (item.y - ysum)**2)
+                dist = np.sqrt((x - xsum)**2 + (y - ysum)**2)
+                
                 # # manhattan 
                 # dist = np.abs(item.x-x) + np.abs(item.y-y)
                 threshold = 2
